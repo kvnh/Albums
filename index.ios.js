270bfc223e90542bb5349782e169f5b982eda623
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-15 08:17:47 +0100

    Making album list scrollable

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,7 @@ import AlbumList from './src/components/AlbumList';
 
 // shortened to...
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
