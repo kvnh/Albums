fca99644e19a35d0ac11b450a65df0a57cf859df
Kevin Hackett <kevinhackett72@gmail.com>
2017-05-15 08:25:30 +0100

    Adding Button component for album detail

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Click Here</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
